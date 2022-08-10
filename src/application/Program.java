e7a7eb61f603509479b4894d44a89004abff7462
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-08-09 22:44:47 -0300

    JDBC - Alterando dados no banco de dados

---
+++ src/application/Program.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+        
+		Connection conn = null;
+		PreparedStatement st = null;
+		//Alterando dados
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+"(DepartmentId = ?)"
+					
+					);
+			
+			
+		
+			
+			st.setDouble(1, 5000.0);
+			st.setInt(2, 1);
+
+
+			int rowsAffected = st.executeUpdate();
+			System.out.println("Done Rows affected: " + rowsAffected);
+			
+		
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			
+			DB.closeStatement(st);
+			
+			DB.closeConnection();
+		}
+		
+	}
+}
